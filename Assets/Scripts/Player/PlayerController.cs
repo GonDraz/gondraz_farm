36ec4502d551025307f4f228024d14fcd7cc2f11
VŨ NGỌC HẢI HÀ <vungochaiha@gmail.com>
2023-04-29 12:41:03 +0700

    Add project files.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform cameraTransform;
+
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float turnSmoothTime;
+
+
+    
+
+    private CharacterController controller;
+    private Animator animator;
+    private Vector2 movement;
+
+    private bool isRunning = false;
+    private float turnSmoothVelocity;
+
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(movement.x, 0f, movement.y).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            
+            if (isRunning)
+            {
+                controller.Move(moveDirection.normalized * runSpeed * Time.deltaTime);
+            }
+            else
+            {
+                controller.Move(moveDirection.normalized * walkSpeed * Time.deltaTime);
+            }
+        }
+        animator.SetFloat("Speed", direction.magnitude);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }    
+    public void Running(InputAction.CallbackContext context)
+    {
+        isRunning = context.performed;
+        animator.SetBool("Running", isRunning);
+    }
+}
